673532e30aa89c6c3f3f8f3186666cea78c9289e
Shengyou Fan <shengyoufan@gmail.com>
2018-10-13 15:08:17 +0800

    建立 ProductsTableSeeder 及撰寫 Seeder 邏輯

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::truncate();
+
+        $faker = \Faker\Factory::create('zh_TW');
+        $total = 20;
+        $units = ['個', '顆', '盒', '台', '粒'];
+
+        foreach (range(1, $total) as $id) {
+            Product::create([
+                'name' => $faker->realText(rand(10, 15)),
+                'price' => rand(20, 10000),
+                'unit' => $units[rand(0, 4)],
+                'description' => $faker->realText(rand(100, 200)),
+                'created_at' => now()->subDays($total - $id)->addHours(rand(1, 5))->addMinutes(rand(1, 5)),
+                'updated_at' => now()->subDays($total - $id)->addHours(rand(6, 10))->addMinutes(rand(10, 30)),
+            ]);
+        }
+    }
+}
