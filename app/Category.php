9f787e283faf0b637a57b4e4ba6804616f0d7bae
Shengyou Fan <shengyoufan@gmail.com>
2018-11-03 15:52:47 +0800

    設定資料表關聯

--- app/Category.php
+++ app/Category.php
@@ -11,4 +11,9 @@ class Category extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -15,4 +15,9 @@ class Product extends Model
         'description',
         'category_id',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
