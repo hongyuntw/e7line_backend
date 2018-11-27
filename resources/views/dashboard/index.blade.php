83d97436c307b368d35ab4e26ac873ff0eb6b739
LeeYK <kevin991239@gmail.com>
2018-11-27 20:48:09 +0800

    1. 移除圖表的部分(取消使用Morris.JS版本)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -96,47 +96,6 @@
         </div>
         <!-- /.row -->
 
-        <!-- 圖表 -->
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Area Chart</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body chart-responsive">
-                        <div class="chart" id="revenue-chart" style="height: 300px;"></div>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <!-- DONUT CHART -->
-                <div class="box box-danger">
-                    <div class="box-header with-border">
-                        <h3 class="box-title">Donut Chart</h3>
-
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                        </div>
-                    </div>
-                    <div class="box-body chart-responsive">
-                        <div class="chart" id="sales-chart" style="height: 300px; position: relative;"></div>
-                    </div>
-                    <!-- /.box-body -->
-                </div>
-                <!-- /.box -->
-            </div>
-        </div>
-        <!-- /.row -->
-
         @if (session('status'))
             <div class="alert alert-success" role="alert">
                 {{ session('status') }}
