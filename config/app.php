9d47487f1b953ad8dd64b57ef495aad2c72c1126
HongYun0901 <harrychiang0@gmail.com>
2018-12-07 23:13:55 +0800

    請下 composer require mpociot/laravel-apidoc-generator

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Mpociot\ApiDoc\ApiDocGeneratorServiceProvider::class,
 
     ],
 
