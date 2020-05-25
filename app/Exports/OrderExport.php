b45df771c8adb2debba20d5f5de6ef13d09b9d47
HongYun0901 <harrychiang0@gmail.com>
2020-05-25 16:36:09 +0800

    order export

--- app/Exports/OrderExport.php
+++ app/Exports/OrderExport.php
@@ -11,13 +11,16 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;     // 自動註冊事件監聽器
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
 
-class OrderExport implements FromArray,WithEvents
+class OrderExport implements FromArray,WithEvents,WithDrawings
 {
 
     protected  $order;
     public static $payment_method_names = ['匯款', '貨到付款', '薪資帳戶扣款', '信用卡刷卡機制', 'LINEPay'];
+    public  $row_index;
 
 
     public function __construct(Order $order)
@@ -25,6 +28,7 @@ class OrderExport implements FromArray,WithEvents
 //        dd($order);
         $this->order  = $order;
 //        $this->order = [];
+        $this->row_index = count($order->order_items) + 29;
 
 
     }
@@ -61,6 +65,25 @@ class OrderExport implements FromArray,WithEvents
                     'J12:J13',
                     'K12:K13',
                 ];
+
+//                logo and qrcode
+                array_push($merge_cell_info, 'C'.($this->row_index) .':E'.($this->row_index+2));
+
+                $event->sheet->getDelegate()->getStyle('C'.($this->row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle('C'.($this->row_index))->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+                array_push($merge_cell_info, 'A'.($this->row_index) .':B'.($this->row_index+2));
+
+                $event->sheet->getDelegate()->getStyle('A'.($this->row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $event->sheet->getDelegate()->getStyle('A'.($this->row_index))->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
+
+
+                $styleArray = array(
+                    'borders' => array(
+                        'outline' => array(
+                            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_MEDIUM,
+                        ),
+                    ),
+                );
                 $event->sheet->getDelegate()->getStyle('A1:Z99')->getFont()->setSize(14)->setName("微軟正黑體");
                 $widths = range(1,100);
                 foreach ($widths as $k => $v) {
@@ -69,6 +92,7 @@ class OrderExport implements FromArray,WithEvents
                 }
 
 //
+                $event->sheet->getDelegate()->getStyle('A1:K1')->getFont()->setSize(20);
                 $event->sheet->getDelegate()->setCellValue('A1','大宗禮券/禮品訂購單');
                 $event->sheet->getDelegate()->setCellValue('A3','★為了保障客戶權益，姓名、電話、地址、收貨時間及付款時間請務必填寫完整★');
                 $event->sheet->getDelegate()->setCellValue('A4','訂購日期');
@@ -115,9 +139,17 @@ class OrderExport implements FromArray,WithEvents
                     $event->sheet->getDelegate()->setCellValue('K'.$row_index,$order_item->quantity*$order_item->price);
                     $row_index+=1;
                 }
+                $event->sheet->getDelegate()->getStyle('A3:K'.$row_index)->getFont()->setSize(12);
                 $row_index++;
                 array_push($merge_cell_info, 'A'.$row_index.':H'.$row_index);
-                $event->sheet->getDelegate()->setCellValue('A'.$row_index,'  ▇ 報帳發票：   統編：後提供');
+                if($this->order->tax_id){
+                    $event->sheet->getDelegate()->setCellValue('A'.$row_index,'  ▇ 報帳發票：   統編：'.$this->order->tax_id);
+
+                }
+                else{
+                    $event->sheet->getDelegate()->setCellValue('A'.$row_index,'  ▇ 報帳發票：   統編： ');
+
+                }
                 $row_index ++;
                 $row_index++;
 
@@ -142,14 +174,14 @@ class OrderExport implements FromArray,WithEvents
 
                 $row_index++;
                 array_push($merge_cell_info, 'A'.($row_index) .':K'.$row_index);
-                $event->sheet->getDelegate()->getRowDimension($row_index)->setRowHeight(60);
+                $event->sheet->getDelegate()->getRowDimension($row_index)->setRowHeight(40);
                 $event->sheet->getDelegate()->getStyle('A'.($row_index))->getAlignment()->setWrapText(true);
                 $event->sheet->getDelegate()->getStyle('A'.($row_index))->getFont()->setSize(12);
                 $event->sheet->getDelegate()->setCellValue('A'.($row_index),'1.訂單須雙方同意商品、價格、數量及交期。雙方窗口簽名後訂單始成立，訂單成立後依序進行商品叫貨及財務流程動作，訂單成立後視同已同意採購不得隨意取消及更改！');
 
                 $row_index++;
                 array_push($merge_cell_info, 'A'.($row_index) .':K'.$row_index);
-                $event->sheet->getDelegate()->getRowDimension($row_index)->setRowHeight(60);
+                $event->sheet->getDelegate()->getRowDimension($row_index)->setRowHeight(40);
                 $event->sheet->getDelegate()->getStyle('A'.($row_index))->getAlignment()->setWrapText(true);
                 $event->sheet->getDelegate()->getStyle('A'.($row_index))->getFont()->setSize(12);
 
@@ -159,6 +191,9 @@ class OrderExport implements FromArray,WithEvents
                 $row_index++;
                 array_push($merge_cell_info, 'A'.($row_index) .':K'.$row_index);
                 $event->sheet->getDelegate()->setCellValue('A'.($row_index),'3.雙方購買得另立買賣合約，保障雙方之權利。');
+                $event->sheet->getDelegate()->getStyle('A'.($row_index-2).':K'.$row_index)->getFont()->setSize(10);
+
+
 
                 $row_index++;
                 array_push($merge_cell_info, 'A'.($row_index) .':K'.$row_index);
@@ -170,10 +205,15 @@ class OrderExport implements FromArray,WithEvents
                 $event->sheet->getDelegate()->setCellValue('A'.($row_index),'付款方式:'.self::$payment_method_names[$this->order->payment_method]);
 
                 array_push($merge_cell_info, 'H'.($row_index) .':I'.$row_index);
+
                 $event->sheet->getDelegate()->getStyle('H'.($row_index))->getFont()->getColor()->setARGB(\PhpOffice\PhpSpreadsheet\Style\Color::COLOR_RED);
 
                 $event->sheet->getDelegate()->setCellValue('H'.($row_index),'(必填欄位匯款日期)');
                 $event->sheet->getDelegate()->getStyle('H'.($row_index))->getFont()->setSize(12);
+                $event->sheet->getDelegate()->getStyle('H'.($row_index) .':I'.$row_index)->applyFromArray($styleArray);
+                $event->sheet->getDelegate()->getStyle('J'.($row_index) .':K'.$row_index)->applyFromArray($styleArray);
+
+
 
 
                 array_push($merge_cell_info, 'J'.($row_index) .':K'.$row_index);
@@ -192,6 +232,11 @@ class OrderExport implements FromArray,WithEvents
                     $event->sheet->getDelegate()->setCellValue('I'.($row_index),'帳號: 046-09-02490-8');
 
                 }
+                array_push($merge_cell_info, 'I'.($row_index) .':K'.$row_index);
+
+
+                $event->sheet->getDelegate()->getStyle('A'.($row_index-1).':K'.$row_index)->getFont()->setSize(10);
+
 
 
                 $row_index++;
@@ -206,18 +251,17 @@ class OrderExport implements FromArray,WithEvents
                 $event->sheet->getDelegate()->setCellValue('A'.($row_index),'聯絡人: '.$this->order->user->name);
 
                 array_push($merge_cell_info, 'F'.($row_index) .':K'.$row_index);
-                $event->sheet->getDelegate()->setCellValue('F'.($row_index),'電話: '.$this->order->user->phone_number.'#'.$this->order->user->extension_number);
+                $event->sheet->getDelegate()->setCellValue('F'.($row_index),'電話: (02)89124000#'.$this->order->user->extension_number);
 
 
                 $row_index++;
                 array_push($merge_cell_info, 'A'.($row_index) .':E'.$row_index);
-                $event->sheet->getDelegate()->setCellValue('A'.($row_index),'傳真號碼: ');
+                $event->sheet->getDelegate()->setCellValue('A'.($row_index),'傳真號碼: 02-89124250 ');
 
                 array_push($merge_cell_info, 'F'.($row_index) .':K'.$row_index);
                 $event->sheet->getDelegate()->setCellValue('F'.($row_index),'Mail: '.$this->order->user->email);
 
-//                合併儲存格
-                $event->sheet->getDelegate()->setMergeCells($merge_cell_info);
+
 
 
                 $event->sheet->getDelegate()->getStyle('A1')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
@@ -261,7 +305,78 @@ class OrderExport implements FromArray,WithEvents
                 $event->sheet->getDelegate()->getStyle('K12')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
                 $event->sheet->getDelegate()->getStyle('K12')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER);
 
+                $row_index = $this->row_index + 3;
+                array_push($merge_cell_info, 'C'.($row_index) .':E'.$row_index);
+                $event->sheet->getDelegate()->setCellValue('C'.($row_index),'企業員工福利平台');
+                $event->sheet->getDelegate()->getStyle('C'.$row_index)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
+
+                array_push($merge_cell_info, 'F'.($row_index-1) .':H'.($row_index-1));
+                $event->sheet->getDelegate()->setCellValue('F'.($row_index-1),'______________________');
+                $event->sheet->getDelegate()->getStyle('F'.($row_index-1))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+
+                array_push($merge_cell_info, 'F'.($row_index) .':H'.$row_index);
+                $event->sheet->getDelegate()->setCellValue('F'.($row_index),'業務窗口');
+                $event->sheet->getDelegate()->getStyle('F'.$row_index)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                array_push($merge_cell_info, 'I'.($row_index) .':K'.$row_index);
+                $event->sheet->getDelegate()->setCellValue('I'.($row_index),'客戶簽收');
+                $event->sheet->getDelegate()->getStyle('I'.$row_index)->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                array_push($merge_cell_info, 'I'.($row_index-1) .':K'.($row_index-1));
+                $event->sheet->getDelegate()->setCellValue('I'.($row_index-1),'______________________');
+                $event->sheet->getDelegate()->getStyle('I'.($row_index-1))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+
+
+
+                $event->sheet->getDelegate()->getStyle('I'.($row_index-3).':K'.($row_index-1))->applyFromArray($styleArray);
+
+                $event->sheet->getDelegate()->getStyle('I'.($row_index-3).':K'.($row_index))->applyFromArray($styleArray);
+
+
+                $row_index += 3;
+                array_push($merge_cell_info, 'A'.($row_index) .':K'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('A'.($row_index),'---------------------------------------------------------內部簽核流程----------------------------------------------------------');
+                $event->sheet->getDelegate()->getStyle('A'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                $row_index++;
+
+                array_push($merge_cell_info, 'A'.($row_index) .':B'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('A'.($row_index),'1.訂單審核');
+                $event->sheet->getDelegate()->getStyle('A'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                array_push($merge_cell_info, 'C'.($row_index) .':D'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('C'.($row_index),'2.採購審核');
+                $event->sheet->getDelegate()->getStyle('C'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+
+                array_push($merge_cell_info, 'E'.($row_index) .':F'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('E'.($row_index),'3.出貨核準');
+                $event->sheet->getDelegate()->getStyle('E'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                array_push($merge_cell_info, 'G'.($row_index) .':H'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('G'.($row_index),'4. 領貨人員');
+                $event->sheet->getDelegate()->getStyle('G'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                array_push($merge_cell_info, 'I'.($row_index) .':J'.($row_index));
+                $event->sheet->getDelegate()->setCellValue('I'.($row_index),'5.收款確認');
+                $event->sheet->getDelegate()->getStyle('I'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+                $event->sheet->getDelegate()->setCellValue('K'.($row_index),'6.印製發票');
+                $event->sheet->getDelegate()->getStyle('K'.($row_index))->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+
+
+                $event->sheet->getDelegate()->getStyle('A'.($row_index).':K'.$row_index)->getFont()->setSize(10);
+
+
+
+
+
+
+
+                //                合併儲存格
+                $event->sheet->getDelegate()->setMergeCells($merge_cell_info);
 
 
 
@@ -271,6 +386,42 @@ class OrderExport implements FromArray,WithEvents
 
     }
 
+    public function drawings()
+    {
+        // TODO: Implement drawings() method.
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('Logo');
+        $drawing->setPath(public_path('/img/e7line/e7lineLogo.png'));
+        $drawing->setHeight(50);
+//        $drawing->setWidth(60);
+//        $drawing->
+        $drawing->setCoordinates('C1');
+
+        $drawing2 = new Drawing();
+        $drawing2->setName('Logo');
+        $drawing2->setDescription('Logo');
+        $drawing2->setPath(public_path('/img/e7line/e7lineLogo.png'));
+        $drawing2->setHeight(80);
+        $drawing2->setWidth(150);
+        $drawing2->setOffsetX(-40);
+
+//        $drawing->
+        $drawing2->setCoordinates('D'.$this->row_index);
+
+        $drawing3 = new Drawing();
+        $drawing3->setName('QRCODE');
+        $drawing3->setDescription('QRCODE');
+        $drawing3->setPath(public_path('/img/e7line/e7lineQRcode.png'));
+        $drawing3->setHeight(100);
+        $drawing3->setWidth(100);
+        $drawing3->setOffsetX(20);
+        $drawing3->setCoordinates('A'.$this->row_index);
+
+
+        return [$drawing,$drawing2,$drawing3];
+
+    }
 
 
 }
